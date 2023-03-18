b7584e8142aa03d13397338bd581b329218189a7
JerichoFletcher <jericho.russel17@gmail.com>
2023-03-18 10:49:43 +0700

    add: ISearchAlgorithm

--- src/BebasFirstLib/Algorithms/ISearchAlgorithm.cs
+++ src/BebasFirstLib/Algorithms/ISearchAlgorithm.cs
@@ -1,8 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace BebasFirstLib.Algorithms {
+﻿namespace BebasFirstLib.Algorithms {
+    /// <summary>
+    /// Antarmuka untuk kelas-kelas algoritma pencarian secara umum.
+    /// </summary>
+    /// <typeparam name="T">Tipe data objek hasil pencarian.</typeparam>
     public interface ISearchAlgorithm<T> {
-
+        /// <summary>
+        /// Melakukan pencarian dengan algoritma tertentu.
+        /// </summary>
+        /// <returns>Objek hasil pencarian.</returns>
+        T Search();
     }
 }
